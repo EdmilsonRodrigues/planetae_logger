9831029b13068e43f45f8bb7159f90e3c9cabe27
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-08-15 20:12:59 +0100

    start creation of logger

--- logger.py
+++ logger.py
@@ -3,17 +3,26 @@ from typing import Any
 
 
 class _LoggerDecorator:
-    def __init__(self, function) -> None:
-        self.function = function
+    def __init__(self, cls) -> None:
+        self.cls = cls
 
-    def __call__(self, *args, **kwargs) -> None:
-        try:
-            print(args, kwargs)
-            result = self.function(*args, **kwargs)
-            self._log_output(result, filename="log.txt")
-        except Exception as e:
-            self._log_output(e, filename="crash_log.txt")
-            raise
+    def __call__(self, *args, **kwargs) -> Any:
+        instance = self.cls(*args, **kwargs)
+        for method in instance:
+            if callable(getattr(self.cls, method)):
+                self.cls.__dict__[method] = self._decorate(getattr(self.cls, method), standard_output=instance.response_output, exception_output=instance.response_output)
+        return
+
+    def _decorate(self, func: Any, standard_output: str, exception_output: str) -> Any:
+        def wrapper(*args, **kwargs) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                self._log_output(result, filename=standard_output)
+                return result
+            except Exception as e:
+                self._log_output(result, filename=exception_output)
+
+        return wrapper
 
     @staticmethod
     def _log_output(output: Any, filename: str) -> None:
@@ -21,6 +30,7 @@ class _LoggerDecorator:
             log_file.write(str(output) + " " + datetime.now().isoformat() + " " + "\n")
 
 
+@_LoggerDecorator
 class Logger:
     standard_output: str
     exceptions_output: str
@@ -29,27 +39,19 @@ class Logger:
         self.standard_output = standard_output
         self.exceptions_output = exceptions_output
 
-    @staticmethod
-    @_LoggerDecorator
-    def log_exception(exception: Exception) -> None:
+    def log_exception(self, exception: Exception) -> None:
         raise exception
 
-    @staticmethod
-    @_LoggerDecorator
-    def log_string(string: str) -> str:
+    def log_string(self, string: str) -> str:
         return string
 
-    @staticmethod
-    @_LoggerDecorator
-    def log_iterable(iterable: list | tuple, name: str = "Array") -> str:
+    def log_iterable(self, iterable: list | tuple, name: str = "Array") -> str:
         string = name + ":\n"
         for i in iterable:
             string += "\t" + str(i)
         return string + "\n"
 
-    @staticmethod
-    @_LoggerDecorator
-    def log_dict(json: dict, name: str = "json") -> str:
+    def log_dict(self, json: dict, name: str = "json") -> str:
         def pprint_json(dictionary: dict | list) -> str:
             pprint_string = ""
             if isinstance(dictionary, dict):
@@ -71,7 +73,5 @@ class Logger:
         string += pprint_json(dictionary=json)
         return string
 
-    @staticmethod
-    @_LoggerDecorator
-    def log_object(obj: object) -> str:
+    def log_object(self, obj: object) -> str:
         return str(obj)
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from logger import Logger
 
+
 logger = Logger(standard_output="logs.txt", exceptions_output="crash_logs.txt")
 logger.log_string("testing 01")
+logger.log_exception(ValueError("Alô som, 1, 2, 3"))
