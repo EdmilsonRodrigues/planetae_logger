8fe65fb1ef8c87242e1355f56ef1ed8e41b58ca9
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-08-18 13:15:46 +0100

    documenting + some improvements

--- logger.py
+++ logger.py
@@ -10,13 +10,13 @@ class _LoggerDecorator:
         instance = self.cls(*args, **kwargs)
         for method in self.cls.__dict__:
             if callable(getattr(self.cls, method)):
-                instance.__dict__[method] = self._decorate(getattr(self.cls, method), instance=instance, standard_output=instance.standard_output, exception_output=instance.exceptions_output)
+                instance.__dict__[method] = self._decorate(getattr(self.cls, method), standard_output=instance.standard_output, exception_output=instance.exceptions_output)
         return instance
 
-    def _decorate(self, func: Any, instance, standard_output: str, exception_output: str) -> Any:
+    def _decorate(self, func: Any, standard_output: str, exception_output: str) -> Any:
         def wrapper(*args, **kwargs) -> Any:
             try:
-                result = func(instance, *args, **kwargs)
+                result = func(*args, **kwargs)
                 self._log_output(result, filename=standard_output)
                 return result
             except Exception as e:
@@ -32,26 +32,78 @@ class _LoggerDecorator:
 
 @_LoggerDecorator
 class Logger:
+    """
+    A class for creating a logger instance
+
+    This class provides methods to log exceptions, strings, iterables, dictionaries,
+    and objects. The logs are stored in specified output strings.
+    """
     standard_output: str
     exceptions_output: str
 
     def __init__(self, standard_output: str, exceptions_output: str) -> None:
+        """
+        Initialise a logger instance
+
+        :param standard_output: The output destination for standard logs.
+        :type standard_output: str
+        :param exceptions_output: The output destination for exception logs.
+        :type exceptions_output: str
+        """
         self.standard_output = standard_output
         self.exceptions_output = exceptions_output
 
-    def log_exception(self, exception: Exception) -> None:
+    @staticmethod
+    def log_exception(exception: Exception) -> None:
+        """
+        Logs an exception to the output destination
+
+        :param exception: The exception to be logged.
+        :type exception: Exception
+        """
         raise exception
 
-    def log_string(self, string: str) -> str:
+    @staticmethod
+    def log_string(string: str) -> str:
+        """
+        Logs a string to the output destination and returns it
+
+        :param string: The string to be logged.
+        :type string: str
+        :return: The logged string.
+        :rtype: str
+        """
         return string
 
-    def log_iterable(self, iterable: list | tuple, name: str = "Array") -> str:
+    @staticmethod
+    def log_iterable(iterable: list | tuple, name: str = "Array") -> str:
+        """
+        logs an iterable to the output destination and returns its string
+        :param iterable: The iterable to be logged.
+        :type iterable: list | tuple
+        :param name: The name of the iterable to be logged.
+        :type name: str
+        :return: The logged iterable.
+        :rtype: str
+        """
         string = name + ":\n"
         for i in iterable:
             string += "\t" + str(i)
         return string + "\n"
 
-    def log_dict(self, json: dict, name: str = "json") -> str:
+    @staticmethod
+    def log_dict(json: dict, name: str = "json") -> str:
+        """
+        Logs a dictionary to the output destination and returns its string
+
+        :param json: The dictionary to be logged.
+        :type json: dict
+        :param name: The name of the dictionary to be logged.
+        :type name: str
+        :return: The logged dictionary.
+        :rtype: str
+
+        """
         def pprint_json(dictionary: dict | list) -> str:
             pprint_string = ""
             if isinstance(dictionary, dict):
@@ -73,5 +125,13 @@ class Logger:
         string += pprint_json(dictionary=json)
         return string
 
-    def log_object(self, obj: object) -> str:
+    @staticmethod
+    def log_object(obj: object) -> str:
+        """
+        Logs an object to the output destination and returns its string
+        :param obj: The object to be logged.
+        :type obj: object
+        :return: The logged object.
+        :rtype: str
+        """
         return str(obj)
